e8b6069fdd821ae4c67aa7dd5909d00c68d2f963
HoangZoan <d.hoang1906@gmail.com>
2021-11-13 13:09:49 +0700

    Small changes

--- login-form/script.js
+++ login-form/script.js
@@ -6,35 +6,39 @@ const forgotPasswordEl = document.querySelector(".forgot-password-text");
 const primaryBtnEl = document.querySelector(".primary-btn");
 const subBtnEl = document.querySelector(".sub-btn");
 
-// Đặt trạng thái hiển thị UI (Có đang ở giao diện Đăng nhập không?)
-let isLoginForm = true;
+const app = () => {
+  // Đặt trạng thái hiển thị UI (Có đang ở giao diện Đăng nhập không?)
+  let isLoginForm = true;
 
-primaryBtnEl.addEventListener("click", () => {
-  if (!isLoginForm) {
-    formCardEl.classList.add("hidden");
-    formNotificationEl.classList.remove("hidden");
-  } else {
+  primaryBtnEl.addEventListener("click", () => {
+    if (!isLoginForm) {
+      formCardEl.classList.add("hidden");
+      formNotificationEl.classList.remove("hidden");
+    } else {
+      isLoginForm = !isLoginForm;
+      renderFormUI();
+    }
+  });
+
+  subBtnEl.addEventListener("click", () => {
     isLoginForm = !isLoginForm;
     renderFormUI();
-  }
-});
+  });
 
-subBtnEl.addEventListener("click", () => {
-  isLoginForm = !isLoginForm;
+  const renderFormUI = () => {
+    formTitleEl.textContent = isLoginForm ? "Đăng nhập" : "Đăng ký";
+    primaryBtnEl.textContent = isLoginForm ? "Đăng nhập" : "Xác nhận đăng ký";
+    subBtnEl.textContent = !isLoginForm ? "Đăng nhập" : "Đăng ký";
+
+    if (isLoginForm) {
+      confirmInputEl.closest(".form-control").classList.add("hidden");
+      forgotPasswordEl.classList.remove("hidden");
+    } else {
+      confirmInputEl.closest(".form-control").classList.remove("hidden");
+      forgotPasswordEl.classList.add("hidden");
+    }
+  };
   renderFormUI();
-});
-
-const renderFormUI = () => {
-  formTitleEl.textContent = isLoginForm ? "Đăng nhập" : "Đăng ký";
-  primaryBtnEl.textContent = isLoginForm ? "Đăng nhập" : "Xác nhận đăng ký";
-  subBtnEl.textContent = !isLoginForm ? "Đăng nhập" : "Đăng ký";
-
-  if (isLoginForm) {
-    confirmInputEl.closest(".form-control").classList.add("hidden");
-    forgotPasswordEl.classList.remove("hidden");
-  } else {
-    confirmInputEl.closest(".form-control").classList.remove("hidden");
-    forgotPasswordEl.classList.add("hidden");
-  }
 };
-renderFormUI();
+
+app();
